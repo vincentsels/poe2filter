1e805fa4ed17ea98deb05704aaccbec001f27fa5
Vincent Sels <vincent_sels@hotmail.com>
2024-12-13 11:23:08 +0100

    Add copy from poe2 trade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'poe2filter';
+
 }
